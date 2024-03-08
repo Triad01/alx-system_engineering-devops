53a6c7004c596a6ebfea67d28108adec429a1407
Triad01 <luckypee01@gmail.com>
2024-03-08 20:10:56 +0100

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,16 @@
-# Installs the Flask package
-package { 'python3-flask':
-    ensure => '2.1.0',
-    provider => 'pip3'
+# install flask version 2.1.0 using pip3
+package { 'python3-pip':
+  ensure => present,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
+}
+
+exec { 'install_werkzeug':
+  command => '/usr/bin/pip3 install Werkzeug==2.1.1',
+  path    => ['/usr/bin'],
+  require => Package['python3-pip'],
 }
