e2d093f70c1d88008c32ac4512839db8530ff820
Triad01 <luckypee01@gmail.com>
2024-03-12 11:05:17 -0700

    advanced task

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#installs a nginx server with custom HTTP header
+
+exec {'add_header':
+  provide     => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
+}
+
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
